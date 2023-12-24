25aa83063650d3e5b27f165d003772fe13797e5b
Kevin Ngo <ktn9699@gmail.com>
2023-12-24 10:34:51 -0500

    finished v1 of game

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const compScore = document.querySelector('.compScore');
+const playerScore = document.querySelector('.playerScore');
+const result = document.querySelector('.result');
+let playerCount = 0;
+let computerCount = 0;
 
+rock.addEventListener('click', () => console.log(playRound(playerSelection = 'Rock')));
+paper.addEventListener('click', () => console.log(playRound(playerSelection = 'Paper')));
+scissors.addEventListener('click', () => console.log(playRound(playerSelection = 'Scissors')));
 
 function getComputerChoice (){ // this gets the computer's choice!
-    const rand = Math.floor(Math.random() * 3);
+    let rand = Math.floor(Math.random() * 3);
     switch (rand) {
         case 0: 
         return 'Rock';
@@ -15,6 +26,7 @@ function getComputerChoice (){ // this gets the computer's choice!
     }
 }
 
+let computerSelection = getComputerChoice();
 function getWinner(playerSelection, computerSelection) {  // get's the winner of the round, returns string result = player, computer or tie.
   let result = ''
   if (playerSelection.toUpperCase() === 'ROCK' && computerSelection.toUpperCase() === 'SCISSORS'
@@ -22,7 +34,7 @@ function getWinner(playerSelection, computerSelection) {  // get's the winner of
   || playerSelection.toUpperCase() === 'SCISSORS' && computerSelection.toUpperCase() === 'PAPER' ) {
     result = 'player';  
     return result; 
-  
+ 
 }
 else if (computerSelection.toUpperCase() === 'ROCK' && playerSelection.toUpperCase() === 'SCISSORS'
 || computerSelection.toUpperCase() === 'PAPER' && playerSelection.toUpperCase() === 'ROCK'
@@ -37,48 +49,38 @@ else {
 }
 }
 
-function playRound(playerSelection, computerSelection) {  // plays the round, returns win/loss message
+function playRound(playerSelection) {  // plays the round, returns win/loss message
+
+  computerSelection = getComputerChoice();
   if (getWinner(playerSelection,computerSelection) === 'player'){
-    return 'You Win! ' + playerSelection + ' beats ' +computerSelection;
+    playerScore.textContent = ++playerCount;
+    result.textContent = 'You Win! ' + playerSelection + ' beats ' + computerSelection+'.'
+
   }
   else if (getWinner(playerSelection, computerSelection) === 'computer'){
-    return 'You lose! ' + computerSelection + ' beats ' +playerSelection;
+    compScore.textContent = ++computerCount;
+    result.textContent = 'You Lose!'
   }
 else {
-  return "It's a tie!";
+
+  result.textContent = "It's a tie!"
+}
+if (playerCount == 5 && playerCount > computerCount) {
+  result.textContent = playerSelection + ' beats ' + computerSelection+" and you have won: " +playerCount+'-'+computerCount;
+playerCount = 0;
+computerCount = 0;
+playerScore.textContent = playerCount;
+compScore.textContent = computerCount;
+}
+if (computerCount == 5 && computerCount > playerCount) {
+  result.textContent = playerSelection + ' beats ' + computerSelection+" and you have won: " +playerCount+'-'+computerCount;
+  playerCount = 0;
+  computerCount = 0;
+  playerScore.textContent = playerCount;
+  compScore.textContent = computerCount;
 }
 }
 
-function game() {
-  let playercount = 0;
-  let computercount = 0;
-  let n = 5; // in a bo3, first to 3, wins, 5 games default. 
-  for (let i = 0; i < n; i++) {
-  let playerSelection = prompt("Rock, paper or scissors?"); // user selects their option
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-    if (getWinner(playerSelection,computerSelection)=== 'player') {
-      playercount++;
 
-    if (playercount == 3) // if playercount reaches 3, they are the winner
-    break;    
-    }
-      else if (getWinner(playerSelection,computerSelection) === 'computer') {
-        computercount++;
-        if (computercount == 3) // if computercount reaches to 3 wins, they are the winner
-        break;    
-      }
-else {
-n++; // if tie, another game is added for play. 
-}
-    }
 
-    if (playercount > computercount) { // if player has 3 wins and has more wins then computer, they are declared the winner
-      return 'You win! The score is: ' + playercount + ' : ' + computercount+ '.';
-    }
-    if (computercount > playercount) { // if computer has 3 wins and has more wins then player, they are declared the winner
-      return 'Computer wins! The score is: ' + computercount + ' : ' + playercount+ '.';
-    } 
-}
 
-console.log(game())
